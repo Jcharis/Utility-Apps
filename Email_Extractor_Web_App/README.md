f9cd83196ebd48e7359e3d7c7dfdafc53ba5b006
JCharis Jesse <jcharistech@gmail.com>
2019-02-13 15:22:47 +0200

    Email Extractor Web App with Flask

---
+++ Email_Extractor_Web_App/README.md
@@ -0,0 +1,11 @@
+### Email Extractor Web App
++ Extract emails and phone numbers from documents using regex
+
+##### Requirement
++ Flask
++ Python
+
+##### .
++ By Jesse E.Agbe(JCharis)
++ Jesus Saves@JCharisTech
++ J-Secur1ty
---
+++ Email_Extractor_Web_App/app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,url_for,request
+import re
+
+email_regex = re.compile(r"[\w\.-]+@[\w\.-]+")
+# phone_num = re.compile(r"^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$")
+# phone_num = re.compile(r"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$")
+# phone_num = re.compile(r"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$")
+phone_num = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/process',methods=["POST"])
+def process():
+	if request.method == 'POST':
+		choice = request.form['taskoption']
+		if choice == 'email':
+			rawtext = request.form['rawtext']
+			results = email_regex.findall(rawtext)
+			num_of_results = len(results)
+		elif choice == 'phone':
+			rawtext = request.form['rawtext']
+			print(rawtext)
+			results = phone_num.findall(rawtext)
+			num_of_results = len(results)
+		
+	
+	return render_template("index.html",results=results,num_of_results = num_of_results)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
